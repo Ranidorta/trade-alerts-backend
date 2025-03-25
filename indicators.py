5a1a0a95f1078120b63ca8bec2c6f92fa5f8c4ce
Ranidorta <ranier.dorta@gmail.com>
2025-03-25 20:55:50 -0300

    Delete indicators.py

--- indicators.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def extract_features(df):
-    df['ma5'] = df['close'].rolling(window=5).mean()
-    df['ma20'] = df['close'].rolling(window=20).mean()
-    df['ma_diff'] = df['ma5'] - df['ma20']
-    df.dropna(inplace=True)
-    return df
