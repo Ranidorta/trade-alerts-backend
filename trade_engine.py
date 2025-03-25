194f77bee54786b12fdc2d8a6cf03558681e94bc
Ranidorta <ranier.dorta@gmail.com>
2025-03-25 20:55:59 -0300

    Delete trade_engine.py

--- trade_engine.py
+++
@@ -1,8 +0,0 @@
-from utils import get_symbols, process_symbol
-from database import save_signal
-from indicators import extract_features
-
-def generate_all_signals():
-    symbols = get_symbols()
-    for symbol in symbols:
-        process_symbol(symbol)
