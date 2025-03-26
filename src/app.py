ffa33e70a63df096b92e9fa8b22b7f1dc7abc173
Ranidorta <ranier.dorta@gmail.com>
2025-03-25 21:45:23 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
-from routes import app  # Importa o app já configurado do routes.py
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Servidor rodando"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
