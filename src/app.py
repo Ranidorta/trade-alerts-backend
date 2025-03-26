f4411c840d9cec65d0f3667f8ca5bfd2150d2f36
Ranidorta <ranier.dorta@gmail.com>
2025-03-25 21:35:36 -0300

    Update and rename app.py to src/app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,5 @@
 from flask import Flask
-from routes import configure_routes
-
-app = Flask(__name__)
-configure_routes(app)
+from routes import app  # Importa o app já configurado do routes.py
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
